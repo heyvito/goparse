a5c8afc7838d847b8b415cf70775627074424951
Victor Gama <hey@vito.io>
2021-05-31 17:20:08 -0300

    fix(abnf): Remove cyclic import

--- test/test_test.go
+++ test/test_test.go
@@ -1,4 +1,4 @@
-package abnf
+package test
 
 import (
 	"fmt"
@@ -8,16 +8,17 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/heyvito/goparse/abnf"
 	"github.com/heyvito/goparse/abnf1"
 )
 
 func TestParseProgressive(t *testing.T) {
-	data, err := os.ReadFile("../test/abnf.abnf")
+	data, err := os.ReadFile("../grammars/abnf.abnf")
 	require.NoError(t, err)
 	n := time.Now()
 	v, err := abnf1.ParseABNFNaive(string(data))
 	diff := time.Since(n)
 	fmt.Printf("Parse took %s\n", diff.String())
 	require.NoError(t, err)
-	fmt.Println(Generate(v))
+	fmt.Println(abnf.Generate(v))
 }
